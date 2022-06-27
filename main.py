550d6c8747a1b40413f1ea0505a7d90400fa2437
213872023 <st21387@ormiston.school.nz>
2022-06-27 20:29:00 +1200

    Commit 3 - Entry's

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ def print_customer_details ():
         Label(main_window, text=(customer_details[name_count][3])).grid(column=4, row=name_count + 8)
         name_count += 1
 
+#Entry's
+def append_name ():
+    global customer_details, entry_Customer_Name,entry_Receipt_Number,entry_Item_Hired,entry_Quantity_Hired, total_entries
+    if len(entry_Customer_Name.get()) != 0 :
+        customer_details.append([entry_Customer_Name.get(),entry_Receipt_Number.get(),entry_Item_Hired.get(),entry_Quantity_Hired.get()])
+        entry_Customer_Name.delete(0,'end')
+        entry_Receipt_Number.delete(0,'end')
+        entry_Item_Hired.delete(0,'end')
+        entry_Quantity_Hired.delete(0,'end')
+        total_entries +=  1
+
 
 
 
