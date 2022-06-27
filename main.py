86f49831e1d7f2e821a18c728a269a43466d55e7
213872023 <st21387@ormiston.school.nz>
2022-06-27 20:24:09 +1200

    Revert "Commit 2 - Labels"
    This reverts commit 3727f044c475c920f91efad982ac8ed420a1ae5b.

--- main.py
+++ main.py
@@ -16,14 +16,6 @@ def print_customer_details ():
     Label(main_window, font='bold',text="Item Hired").grid(column=3,row=7)
     Label(main_window, font='bold',text="Quantity Hired").grid(column=4,row=7)
 
-    while name_count < total_entries:
-        Label(main_window, text=name_count).grid(column=0, row=name_count + 8)
-        Label(main_window, text=(customer_details[name_count][0])).grid(column=1, row=name_count + 8)
-        Label(main_window, text=(customer_details[name_count][1])).grid(column=2, row=name_count + 8)
-        Label(main_window, text=(customer_details[name_count][2])).grid(column=3, row=name_count + 8)
-        Label(main_window, text=(customer_details[name_count][3])).grid(column=4, row=name_count + 8)
-        name_count += 1
-
 
 
 
