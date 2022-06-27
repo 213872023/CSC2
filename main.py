35672323880806728c6bbec0b270295b5daff32d
213872023 <st21387@ormiston.school.nz>
2022-06-27 20:20:49 +1200

    Commit 1 - Text Labels

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from tkinter import *
-global entry_customer_name
-mw = Tk()
-print("test")
+
+from tkinter import ttk
+
+#quit
+def quit():
+    main_window()
+
+#Labels
+def print_customer_details ():
+    global j_names, total_entries, name_count
+    name_count = 0
+    Label(main_window, font='bold',text="Row").grid(column=0,row=7)
+    Label(main_window, font='bold',text="Customer Name").grid(column=1,row=7)
+    Label(main_window, font='bold',text="Receipt Number").grid(column=2,row=7)
+    Label(main_window, font='bold',text="Item Hired").grid(column=3,row=7)
+    Label(main_window, font='bold',text="Quantity Hired").grid(column=4,row=7)
 
 
 
 
 
-mw.mainloop()
+
+
